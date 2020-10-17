99c5a9aa5f5dc7dc916b78961395bd2db4f3d3c7
Ender Cypher <cqualls03@hotmail.com>
2020-10-16 23:35:54 -0500

    Create ipsw-grab.sh

---
+++ ipsw-grab.sh
@@ -0,0 +1,18 @@
+mkdir -p iPSW/Beta/{iPhone-SE2,iPhone-11,iPhone-11-Pro,iPhone-11-Pro-Max,iPhone-XS,iPhone-XS-Max,iPhone-XR,iPhone-X-GSM,iPhone-X-Global,iPhone-8-Plus-GSM,iPhone-8-Plus-Global,iPhone-8-GSM,iPhone-8-Global,iPhone-7-Plus-GSM,iPhone-7-GSM,iPhone-7-Plus-Global,iPhone-7-Global,iPhone-6S+,iPhone-6S,iPhone-SE}
+
+wget -i ./iPhone-SE  -P /iPSW/Beta/iPhone-SE
+wget -i ./iPhone-6S -P /iPSW/Beta/iPhone-6S
+wget -i ./iPhone-6S-Plus -P /iPSW/Beta/iPhone-6S+/
+wget -i ./iPhone-7 -P /iPSW/Beta/iPhone-7/
+wget -i ./iPhone-7-Plus -P /iPSW/Beta/iPhone-7+/
+wget -i ./iPhone-8 -P /iPSW/Beta/iPhone-8/
+wget -i ./iPhone-8-Plus -P /iPSW/Beta/iPhone-8+/
+wget -i ./iPhone-X-GSM -P /iPSW/Beta/iPhone-X-GSM
+wget -i ./iPhone-X-Global -P /iPSW/Beta/iPhone-X-Global
+wget -i ./iPhone-XR -P /iPSW/Beta/iPhone-XR
+wget -i ./iPhone-XS -P /iPSW/Beta/iPhone-XS
+wget -i ./iPhone-XS-Max -P /iPSW/Beta/iPhone-XS-Max
+wget -i ./iPhone-11 -P /iPSW/Beta/iPhone-11
+wget -i ./iPhone-11-Pro -P /iPSW/Beta/iPhone-11-Pro
+wget -i ./iPhone-11-Pro-Max -P /iPSW/Beta/iPhone-11-Pro-Max
+wget -i ./iPhone-SE2  -P /iPSW/Beta/iPhone-SE2
